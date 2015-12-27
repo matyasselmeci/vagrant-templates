8d55584587db965b996d22e673185a21f78b238a
Matyas Selmeci <matyas@cs.wisc.edu>
2015-12-27 16:43:02 -0600

    run git commit after etckeeper init

--- provisionscripts/provision-etckeeper.sh
+++ provisionscripts/provision-etckeeper.sh
@@ -3,5 +3,5 @@
 rpm -q etckeeper || yum install -y -d1 etckeeper
 rpm -q git || yum install -y -d1 git
 
-(cd /etc; etckeeper init) || :
+(cd /etc; etckeeper init; git commit -m "initial commit") || :
 
