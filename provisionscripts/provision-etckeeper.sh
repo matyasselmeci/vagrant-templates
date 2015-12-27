aa16299f0ae2eb8cd4849da255990ec5384fb79f
Matyas Selmeci <matyas@cs.wisc.edu>
2015-12-27 16:57:14 -0600

    etckeeper: set up git and run git gc

--- provisionscripts/provision-etckeeper.sh
+++ provisionscripts/provision-etckeeper.sh
@@ -3,5 +3,9 @@
 rpm -q etckeeper || yum install -y -d1 etckeeper
 rpm -q git || yum install -y -d1 git
 
-(cd /etc; etckeeper init; git commit -m "initial commit") || :
+git config --global --get user.name &> /dev/null || (
+    git config --global user.name "root"
+    git config --global user.email "root@$(hostname -f)"
+)
+(etckeeper init; etckeeper commit "initial commit"; etckeeper vcs gc) || :
 
