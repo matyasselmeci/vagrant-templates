ef2cdcaf7e12a28e36d2601e6000e086440b1dc6
Matyas Selmeci <matyas@cs.wisc.edu>
2015-12-28 16:47:47 -0600

    Shut up etckeeper

--- provisionscripts/provision-etckeeper.sh
+++ provisionscripts/provision-etckeeper.sh
@@ -7,5 +7,5 @@ git config --global --get user.name &> /dev/null || (
     git config --global user.name "root"
     git config --global user.email "root@$(hostname -f)"
 )
-(etckeeper init; etckeeper commit "initial commit"; etckeeper vcs gc) || :
+(etckeeper init; etckeeper commit "initial commit"; etckeeper vcs gc) > /dev/null || :
 
