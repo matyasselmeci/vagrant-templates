b489d3227f679fd0f871fc4590272da991948d07
Matyas Selmeci <matyas@cs.wisc.edu>
2019-10-04 16:59:08 -0500

    10 uids/gids is enough...?

--- _provision/scripts/provision-buildah.sh
+++ _provision/scripts/provision-buildah.sh
@@ -8,6 +8,6 @@ echo "user.max_user_namespaces=10000" > /etc/sysctl.d/99-namespaces.conf
 sysctl user.max_user_namespaces=10000
 
 touch /etc/sub{u,g}id
-usermod --add-subuids 10000-75535 vagrant
-usermod --add-subgids 10000-75535 vagrant
+usermod --add-subuids 10000-10010 vagrant
+usermod --add-subgids 10000-10010 vagrant
 
