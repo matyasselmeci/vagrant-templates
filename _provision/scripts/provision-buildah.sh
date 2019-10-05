bd396d464c33ad5d728c08eeea53c40878383203
Matyas Selmeci <matyas@cs.wisc.edu>
2019-10-04 20:42:58 -0500

    Need at least 200 UIDs/GIDs but no harm in using 1000; add ansible version of the provisioner and a template

--- _provision/scripts/provision-buildah.sh
+++ _provision/scripts/provision-buildah.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 yum install -y epel-release
 yum install -y buildah shadow-utils podman podman-docker
 
@@ -8,6 +7,11 @@ echo "user.max_user_namespaces=10000" > /etc/sysctl.d/99-namespaces.conf
 sysctl user.max_user_namespaces=10000
 
 touch /etc/sub{u,g}id
-usermod --add-subuids 10000-10010 vagrant
-usermod --add-subgids 10000-10010 vagrant
+# need 200 for centos:7 and centos:8
+# There doesn't seem to be a limit -- each user on my laptop uses 65k --
+# so might as well use 1000.
+# if you get lchown invalid argument errors, increase this,
+# log out, and log back in
+usermod --add-subuids 10000-10999 vagrant
+usermod --add-subgids 10000-10999 vagrant
 
