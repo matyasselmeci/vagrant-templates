20bda23d84825dd3a1386bc92d59eb13762d1870
Matyas Selmeci <matyas@cs.wisc.edu>
2019-10-30 15:41:30 -0500

    Use curl to get newshell and publicutils

--- _provision/scripts/provision-matyas.sh
+++ _provision/scripts/provision-matyas.sh
@@ -4,40 +4,30 @@ utils_root=/usr/local/bin
 utils_path=$utils_root/utils:$utils_root/utils/tiny
 newshell_path=/usr/local/share/newshell
 
-yum install -y -d1 git zsh wget
+yum install -y -d1 git zsh curl
 
 echo "export PATH=$utils_path:\$PATH" > /etc/profile.d/utils.sh
 echo "[ -d $utils_path/shellfn ] && for x in $utils_path/shellfn/*.sh; do . \$x || : ; done" >> /etc/profile.d/utils.sh
 echo "setenv PATH $utils_path:\$PATH" > /etc/profile.d/utils.csh
 
 pushd $utils_root
-#git clone http://pages.cs.wisc.edu/~matyas/utils.git
-git clone https://git.doit.wisc.edu/MATYAS/publicutils.git
-#for script in mr vcsh; do
-#    if wget http://pages.cs.wisc.edu/~matyas/$script; then
-#        chmod +x $script
-#    fi
-#done
+curl -Ls https://git.doit.wisc.edu/MATYAS/publicutils/-/archive/master/publicutils-master.tar.gz | tar -xz && \
+    mv publicutils-master* utils
 popd
 
-#git clone http://pages.cs.wisc.edu/~matyas/newshell.git $newshell_path
-git clone https://git.doit.wisc.edu/MATYAS/newshell.git $newshell_path
+mkdir -p $(dirname $newshell_path)
+curl -Ls https://git.doit.wisc.edu/MATYAS/newshell/-/archive/master/newshell-master.tar.gz | tar -xz && \
+    mv newshell-master* $newshell_path
 $newshell_path/install.sh ~
 $newshell_path/install.sh ~vagrant
 
-wget -nc -O ~/.screenrc http://pages.cs.wisc.edu/~matyas/screenrc
-wget -nc -O ~vagrant/.screenrc http://pages.cs.wisc.edu/~matyas/screenrc
+curl -Lso ~/.screenrc http://pages.cs.wisc.edu/~matyas/screenrc
+cp ~/.screenrc ~vagrant/.screenrc
+
+chown -R vagrant: ~vagrant
 
 if [[ -x /bin/zsh ]]; then
     chsh -s /bin/zsh root
     chsh -s /bin/zsh vagrant
 fi
-
-#first_if=$($utils_path/list-eth-interfaces | head -n 1)
-#if [[ $? == 0 && -n $first_if ]]; then
-#    ipaddr=$($utils_path/get-ip-address $first_if)
-#    if [[ $? == 0 && -n $ipaddr ]]; then
-#        echo "$ipaddr `cat /etc/hostname`" >> /etc/hosts
-#    fi
-#fi
 
