d6b1bd663dac872359fbd36edd8639ab4da17e61
Matyas Selmeci <matyas@cs.wisc.edu>
2021-03-16 17:11:34 -0500

    moar uids....

--- _provision/scripts/provision-buildah.sh
+++ _provision/scripts/provision-buildah.sh
@@ -7,11 +7,6 @@ echo "user.max_user_namespaces=10000" > /etc/sysctl.d/99-namespaces.conf
 sysctl user.max_user_namespaces=10000
 
 touch /etc/sub{u,g}id
-# need 200 for centos:7 and centos:8
-# There doesn't seem to be a limit -- each user on my laptop uses 65k --
-# so might as well use 1000.
-# if you get lchown invalid argument errors, increase this,
-# log out, and log back in
-usermod --add-subuids 10000-10999 vagrant
-usermod --add-subgids 10000-10999 vagrant
+usermod --add-subuids 100000-165535 vagrant
+usermod --add-subgids 100000-165535 vagrant
 
