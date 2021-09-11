62dbd217a9444b289130b4206dddc3a8a5d5a840
Matyas Selmeci <matyas@cs.wisc.edu>
2021-09-10 22:24:51 -0500

    Add utils/tiny to PATH

--- _provision/ansible-roles/matyas/files/utils.sh
+++ _provision/ansible-roles/matyas/files/utils.sh
@@ -1,4 +1,4 @@
-export PATH=/usr/local/bin/utils:$PATH
+export PATH=/usr/local/bin/utils:/usr/local/bin/utils/tiny:$PATH
 if [ -d /usr/local/bin/utils/shellfn ]; then
     for x in /usr/local/bin/utils/shellfn/*.sh; do
         . $x
